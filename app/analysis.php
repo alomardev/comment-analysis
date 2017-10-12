e237e7826397b3007a59ef2b7909c38aed01cd8b
Abdulrahman Alomar <alomardev@gmail.com>
2017-10-12 12:14:51 +0300

    view done

--- app/analysis.php
+++ app/analysis.php
@@ -3,7 +3,6 @@
 <h1>Analysis</h1>
 
 <div class="row">
-	
 	<div class='chart-wrapper col-md-6 pb-3'>
 		<div class="card">
 			<div class="card-body">
@@ -17,7 +16,44 @@
 					
 					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
 					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
+
+					<line class='chart-sep-line' x1="0" x2="100%" y1="5%" y2="5%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="35%" y2="35%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="65%" y2="65%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="95%" y2="95%" />
+
+					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
+					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
+				</svg>
+				<div class="clearfix">
+					<small class="float-right">
+						<span class="text-success">1234+</span>
+						<span class="text-danger">123-</span>
+					</small>
+					<small class="float-left">Total comments: 1000</small>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class='chart-wrapper col-md-6 pb-3'>
+		<div class="card">
+			<div class="card-body">
+				<h5 class='ellipsis'>Database Design and Analysis</h5>
+				<svg>
+					<rect class='chart-block-natural' y="5%" height="30%" width="10%" />
+					<text class='chart-text-natural' y="20%" x="11%">10%</text>
+
+					<rect class='chart-block-negative' y="35%" height="30%" width="40%" />
+					<text class='chart-text-negative' y="50%" x="41%">40%</text>
 					
+					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
+					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
+
+					<line class='chart-sep-line' x1="0" x2="100%" y1="5%" y2="5%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="35%" y2="35%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="65%" y2="65%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="95%" y2="95%" />
+
 					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
 					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
 				</svg>
@@ -32,7 +68,6 @@
 		</div>
 	</div>
 
-	
 	<div class='chart-wrapper col-md-6 pb-3'>
 		<div class="card">
 			<div class="card-body">
@@ -46,7 +81,44 @@
 					
 					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
 					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
+
+					<line class='chart-sep-line' x1="0" x2="100%" y1="5%" y2="5%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="35%" y2="35%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="65%" y2="65%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="95%" y2="95%" />
+
+					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
+					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
+				</svg>
+				<div class="clearfix">
+					<small class="float-right">
+						<span class="text-success">1234+</span>
+						<span class="text-danger">123-</span>
+					</small>
+					<small class="float-left">Total comments: 1000</small>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class='chart-wrapper col-md-6 pb-3'>
+		<div class="card">
+			<div class="card-body">
+				<h5 class='ellipsis'>Database Design and Analysis</h5>
+				<svg>
+					<rect class='chart-block-natural' y="5%" height="30%" width="10%" />
+					<text class='chart-text-natural' y="20%" x="11%">10%</text>
+
+					<rect class='chart-block-negative' y="35%" height="30%" width="40%" />
+					<text class='chart-text-negative' y="50%" x="41%">40%</text>
 					
+					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
+					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
+
+					<line class='chart-sep-line' x1="0" x2="100%" y1="5%" y2="5%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="35%" y2="35%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="65%" y2="65%" />
+					<line class='chart-sep-line' x1="0" x2="100%" y1="95%" y2="95%" />
+
 					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
 					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
 				</svg>
@@ -61,63 +133,6 @@
 		</div>
 	</div>
 
-
-	<div class='chart-wrapper col-md-6 pb-3'>
-		<div class="card">
-			<div class="card-body">
-				<h5 class='ellipsis'>Database Design and Analysis</h5>
-				<svg>
-					<rect class='chart-block-natural' y="5%" height="30%" width="10%" />
-					<text class='chart-text-natural' y="20%" x="11%">10%</text>
-
-					<rect class='chart-block-negative' y="35%" height="30%" width="40%" />
-					<text class='chart-text-negative' y="50%" x="41%">40%</text>
-					
-					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
-					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
-					
-					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
-					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
-				</svg>
-				<div class="clearfix">
-					<small class="float-right">
-						<span class="text-success">1234+</span>
-						<span class="text-danger">123-</span>
-					</small>
-					<small class="float-left">Total comments: 1000</small>
-				</div>
-			</div>
-		</div>
-	</div>
-
-		
-	<div class='chart-wrapper col-md-6 pb-3'>
-		<div class="card">
-			<div class="card-body">
-				<h5 class='ellipsis'>Database Design and Analysis</h5>
-				<svg>
-					<rect class='chart-block-natural' y="5%" height="30%" width="10%" />
-					<text class='chart-text-natural' y="20%" x="11%">10%</text>
-
-					<rect class='chart-block-negative' y="35%" height="30%" width="40%" />
-					<text class='chart-text-negative' y="50%" x="41%">40%</text>
-					
-					<rect class='chart-block-positive' y="65%" height="30%" width="100%" />
-					<text class='chart-text-positive chart-text-inner' y="80%" x="98%">100%</text>
-					
-					<line class='chart-line' x1="0" x2="0" y1="0" y2="100%" />
-					<line class='chart-line' x1="0" x2="100%" y1="100%" y2="100%" />
-				</svg>
-				<div class="clearfix">
-					<small class="float-right">
-						<span class="text-success">1234+</span>
-						<span class="text-danger">123-</span>
-					</small>
-					<small class="float-left">Total comments: 1000</small>
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
 
 @@include("./_bottom.php", {"scripts": []})
